kirsan31/winforms-datavisualization

commit 5fc2783d59c5b118c033b331e88b1e58da7ac213

KeywordsStringEditor port completed.

src/WinForms.DataVisualization.Designer.ClientServerProtocol/Endpoints/KeywordsStringEditor/KeywordsStringEditorEditValueResponse.cs
@@ -1,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.DotNet.DesignTools.Protocol.DataPipe;
+using Microsoft.DotNet.DesignTools.Protocol.Endpoints;
+
+using System.Windows.Forms.DataVisualization.Charting.Utilities;
+
+namespace WinForms.DataVisualization.Designer.Protocol.Endpoints
+{
+    public class KeywordsStringEditorEditValueResponse : Response
+    {
+        public bool IsEmpty => RegisteredKeywords is null && MaxYValueNumber == 0;
+        internal IReadOnlyList<KeywordInfo>? RegisteredKeywords { get; private set; }
+        public int MaxYValueNumber { get; private set; }
+
+
+        public KeywordsStringEditorEditValueResponse() { }
+
+        public KeywordsStringEditorEditValueResponse(IDataPipeReader reader) : base(reader) { }
+
+        internal KeywordsStringEditorEditValueResponse(IReadOnlyList<KeywordInfo>? registeredKeywords, int maxYValueNumber)
+        {
+            RegisteredKeywords = registeredKeywords;
+            MaxYValueNumber = maxYValueNumber;
+        }
+
+        protected override void ReadProperties(IDataPipeReader reader)
+        {
+            RegisteredKeywords = reader.ReadDataPipeObjectArray<KeywordInfo>(nameof(RegisteredKeywords));
+            MaxYValueNumber = reader.ReadInt32(nameof(MaxYValueNumber));
+        }
+
+        protected override void WriteProperties(IDataPipeWriter writer)
+        {
+            writer.WriteDataPipeObjectArray(nameof(RegisteredKeywords), RegisteredKeywords);
+            writer.Write(nameof(MaxYValueNumber), MaxYValueNumber);
+        }
+    }
+}

src/WinForms.DataVisualization.Designer.Server/KeywordsStringEditorEditValueHandler.cs
@@ -1,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms.DataVisualization.Charting.Utilities;
+
+using Microsoft.DotNet.DesignTools.Protocol.Endpoints;
+
+using WinForms.DataVisualization.Designer.Protocol.Endpoints;
+
+namespace WinForms.DataVisualization.Designer.Server;
+
+[ExportRequestHandler(EndpointNames.KeywordsStringEditorEditValue)]
+internal class KeywordsStringEditorEditValueHandler : RequestHandler<KeywordsStringEditorEditValueRequest, KeywordsStringEditorEditValueResponse>
+{
+    public override KeywordsStringEditorEditValueResponse HandleRequest(KeywordsStringEditorEditValueRequest request)
+    {
+        var editingObj = request.EditingObj;
+        if (editingObj is null)
+            return new KeywordsStringEditorEditValueResponse();
+
+        // Try getting access to the associated series
+        Series? series = null;
+        Chart? chart = null;
+
+        // Check object editingObj edited
+        if (editingObj is Series series1)
+        {
+            series = series1;
+        }
+        else if (editingObj is DataPoint point)
+        {
+            series = point.series;
+        }
+        else if (editingObj is LegendItem item)
+        {
+            if (item.Common != null)
+            {
+                chart = item.Common.Chart;
+                if (item.Common.DataManager.Series.IndexOf(item.SeriesName) >= 0)
+                {
+                    series = item.Common.DataManager.Series[item.SeriesName];
+                }
+            }
+        }
+        else if (editingObj is LegendCellColumn column)
+        {
+            if (column.Legend is not null)
+            {
+                chart = column.Legend.Common.Chart;
+            }
+        }
+        else if (editingObj is Annotation annotation)
+        {
+            chart = annotation.Chart;
+            if (annotation.AnchorDataPoint is not null)
+            {
+                series = annotation.AnchorDataPoint.series;
+            }
+            else if (chart is not null && chart.Series.Count > 0)
+            {
+                series = chart.Series[0];
+            }
+        }
+
+        //Make sure chart reference was found
+        if (chart is null && series is not null)
+        {
+            chart = series.Chart;
+        }
+
+        // Get maximum number of Y values
+        int maxYValueNumber = 9;
+        if (series is not null)
+        {
+            maxYValueNumber = series.YValuesPerPoint - 1;
+        }
+        else if (chart is not null)
+        {
+            // Find MAX number of Y values use in all series
+            maxYValueNumber = 0;
+            foreach (Series ser in chart.Series)
+            {
+                maxYValueNumber = Math.Max(maxYValueNumber, ser.YValuesPerPoint - 1);
+            }
+        }
+
+        return new KeywordsStringEditorEditValueResponse((chart?.GetService(typeof(KeywordsRegistry)) as KeywordsRegistry)?.registeredKeywords.AsReadOnly(), maxYValueNumber);
+    }
+}

src/WinForms.DataVisualization.Utilities/Utilities.csproj
@@ -2,6 +2,7 @@
 	<PropertyGroup>
 		<TargetFramework>netstandard2.0</TargetFramework>
 		<UseWindowsForms>true</UseWindowsForms>
+		<UseDesignerSdk>true</UseDesignerSdk>
 		<LangVersion>latest</LangVersion>
 		<AssemblyName>WinForms.DataVisualization.Utilities</AssemblyName>
 		<Nullable>enable</Nullable>
@@ -14,6 +15,8 @@
 	<ItemGroup>
 		<InternalsVisibleTo Include="WinForms.DataVisualization" />
 		<InternalsVisibleTo Include="WinForms.DataVisualization.Designer.Client" />
+		<InternalsVisibleTo Include="WinForms.DataVisualization.Designer.Server" />
+		<InternalsVisibleTo Include="WinForms.DataVisualization.Designer.ClientServerProtocol" />
 	</ItemGroup>
 
 	<ItemGroup Label="Embedded Resources">

src/System.Windows.Forms.DataVisualization/ChartWinControl.cs
@@ -2917,21 +2917,10 @@ namespace System.Windows.Forms.DataVisualization.Charting
         public new object GetService(Type serviceType)
         {
             // Check arguments
-            if (serviceType == null)
+            if (serviceType is null)
                 throw new ArgumentNullException(nameof(serviceType));
 
-            object service = null;
-            if (serviceContainer != null)
-            {
-                service = serviceContainer.GetService(serviceType);
-            }
-
-            if (service == null)
-            {
-                base.GetService(serviceType);
-            }
-
-            return service;
+            return serviceContainer?.GetService(serviceType) ?? base.GetService(serviceType);
         }
 
         /// <summary>
@@ -2946,7 +2935,7 @@ namespace System.Windows.Forms.DataVisualization.Charting
         /// CallOnFormatNumber.
         /// </summary>
         /// <param name="caller">Event caller. Can be ChartPicture, ChartArea or Legend objects.</param>
-        /// <param name="e">Event arguemtns</param>
+        /// <param name="e">Event arguments</param>
         private void OnFormatNumber(object caller, FormatNumberEventArgs e)
         {
             if (FormatNumber != null)

src/WinForms.DataVisualization.Designer.Client/DataVisualization.Client.csproj
@@ -26,8 +26,8 @@
 		<Compile Update="TypeEditors\KeywordEditor.cs">
 			<SubType>Form</SubType>
 		</Compile>
-		<Compile Update="TypeEditors\KeywordsStringEditor.cs">
-			<SubType>Form</SubType>
+		<Compile Update="TypeEditors\KeywordsStringEditorForm.cs">
+		  <SubType>Form</SubType>
 		</Compile>
 	</ItemGroup>
 </Project>

src/WinForms.DataVisualization.Designer.Client/TypeEditors/KeywordsStringEditorForm.Designer.cs
@@ -1,0 +1,202 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+
+//
+//  Purpose:	Design-time editor for the strings that may contain
+//				keywords. Form automatically retrieves the list of 
+//				recognizable keywords from the chart keywords 
+//				registry.
+//
+
+
+using System.Collections;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms.DataVisualization.Charting.Utilities;
+using System;
+using System.Collections.Generic;
+
+namespace WinForms.DataVisualization.Designer.Client
+{
+    /// <summary>
+    /// String editor form that is used to edit properties that support keywords.
+    /// </summary>
+    internal partial class KeywordsStringEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container _components;
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._richTextBox = new System.Windows.Forms.RichTextBox();
+            this._groupBoxString = new System.Windows.Forms.GroupBox();
+            this._buttonEdit = new System.Windows.Forms.Button();
+            this._buttonInsert = new System.Windows.Forms.Button();
+            this._buttonOk = new System.Windows.Forms.Button();
+            this._buttonCancel = new System.Windows.Forms.Button();
+            this._labelDescription = new System.Windows.Forms.Label();
+            this._panelOkCancelButtons = new System.Windows.Forms.Panel();
+            this._panelTopContent = new System.Windows.Forms.Panel();
+            this._panelInsertEditButtons = new System.Windows.Forms.Panel();
+            this._groupBoxString.SuspendLayout();
+            this._panelOkCancelButtons.SuspendLayout();
+            this._panelTopContent.SuspendLayout();
+            this._panelInsertEditButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this._richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._richTextBox.Location = new System.Drawing.Point(6, 19);
+            this._richTextBox.Margin = new System.Windows.Forms.Padding(7);
+            this._richTextBox.Name = "_richTextBox";
+            this._richTextBox.Size = new System.Drawing.Size(488, 106);
+            this._richTextBox.TabIndex = 0;
+            this._richTextBox.WordWrap = false;
+            this._richTextBox.SelectionChanged += new System.EventHandler(this.richTextBox_SelectionChanged);
+            this._richTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox_KeyDown);
+            this._richTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox_KeyPress);
+            this._richTextBox.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
+            // 
+            // groupBoxString
+            // 
+            this._groupBoxString.Controls.Add(this._panelInsertEditButtons);
+            this._groupBoxString.Controls.Add(this._richTextBox);
+            this._groupBoxString.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._groupBoxString.Location = new System.Drawing.Point(0, 56);
+            this._groupBoxString.Name = "_groupBoxString";
+            this._groupBoxString.Padding = new System.Windows.Forms.Padding(6);
+            this._groupBoxString.Size = new System.Drawing.Size(500, 131);
+            this._groupBoxString.TabIndex = 1;
+            this._groupBoxString.TabStop = false;
+            this._groupBoxString.Text = SR.LabelStringWithKeywords;
+            // 
+            // buttonEdit
+            // 
+            this._buttonEdit.Enabled = false;
+            this._buttonEdit.Location = new System.Drawing.Point(30, 34);
+            this._buttonEdit.Name = "_buttonEdit";
+            this._buttonEdit.Size = new System.Drawing.Size(156, 27);
+            this._buttonEdit.TabIndex = 2;
+            this._buttonEdit.Text = SR.LabelEditKeyword;
+            this._buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonInsert
+            // 
+            this._buttonInsert.Location = new System.Drawing.Point(30, 2);
+            this._buttonInsert.Name = "_buttonInsert";
+            this._buttonInsert.Size = new System.Drawing.Size(156, 27);
+            this._buttonInsert.TabIndex = 1;
+            this._buttonInsert.Text = SR.LabelInsertNewKeyword;
+            this._buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonOk
+            // 
+            this._buttonOk.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            this._buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this._buttonOk.Location = new System.Drawing.Point(305, 9);
+            this._buttonOk.Name = "_buttonOk";
+            this._buttonOk.Size = new System.Drawing.Size(90, 27);
+            this._buttonOk.TabIndex = 2;
+            this._buttonOk.Text = SR.LabelButtonOk;
+            this._buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonCancel
+            // 
+            this._buttonCancel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            this._buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this._buttonCancel.Location = new System.Drawing.Point(401, 9);
+            this._buttonCancel.Name = "_buttonCancel";
+            this._buttonCancel.Size = new System.Drawing.Size(90, 27);
+            this._buttonCancel.TabIndex = 3;
+            this._buttonCancel.Text = SR.LabelButtonCancel;
+            // 
+            // labelDescription
+            // 
+            this._labelDescription.Dock = System.Windows.Forms.DockStyle.Top;
+            this._labelDescription.Location = new System.Drawing.Point(0, 0);
+            this._labelDescription.Name = "_labelDescription";
+            this._labelDescription.Size = new System.Drawing.Size(500, 56);
+            this._labelDescription.TabIndex = 0;
+            this._labelDescription.Text = SR.DesciptionCustomLabelEditorTitle;
+            // 
+            // _panelOkCancelButtons
+            // 
+            this._panelOkCancelButtons.Controls.Add(this._buttonOk);
+            this._panelOkCancelButtons.Controls.Add(this._buttonCancel);
+            this._panelOkCancelButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this._panelOkCancelButtons.Location = new System.Drawing.Point(4, 191);
+            this._panelOkCancelButtons.Name = "_panelOkCancelButtons";
+            this._panelOkCancelButtons.Padding = new System.Windows.Forms.Padding(6);
+            this._panelOkCancelButtons.Size = new System.Drawing.Size(500, 44);
+            this._panelOkCancelButtons.TabIndex = 4;
+            // 
+            // _panelTopContent
+            // 
+            this._panelTopContent.Controls.Add(this._groupBoxString);
+            this._panelTopContent.Controls.Add(this._labelDescription);
+            this._panelTopContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._panelTopContent.Location = new System.Drawing.Point(4, 4);
+            this._panelTopContent.Name = "_panelTopContent";
+            this._panelTopContent.Size = new System.Drawing.Size(500, 187);
+            this._panelTopContent.TabIndex = 5;
+            // 
+            // _panelInsertEditButtons
+            // 
+            this._panelInsertEditButtons.Controls.Add(this._buttonInsert);
+            this._panelInsertEditButtons.Controls.Add(this._buttonEdit);
+            this._panelInsertEditButtons.Dock = System.Windows.Forms.DockStyle.Right;
+            this._panelInsertEditButtons.Location = new System.Drawing.Point(305, 19);
+            this._panelInsertEditButtons.Name = "_panelInsertEditButtons";
+            this._panelInsertEditButtons.Size = new System.Drawing.Size(189, 106);
+            this._panelInsertEditButtons.TabIndex = 3;
+            // 
+            // KeywordsStringEditorForm
+            // 
+            this.CancelButton = this._buttonCancel;
+            this.ClientSize = new System.Drawing.Size(524, 275);
+            this.Controls.Add(this._panelTopContent);
+            this.Controls.Add(this._panelOkCancelButtons);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(524, 275);
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Name = "KeywordsStringEditorForm";
+            this.Text = SR.LabelStringKeywordsEditor;
+            this.Load += new System.EventHandler(this.KeywordsStringEditorForm_Load);
+            this._groupBoxString.ResumeLayout(false);
+            this._panelOkCancelButtons.ResumeLayout(false);
+            this._panelTopContent.ResumeLayout(false);
+            this._panelInsertEditButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private System.Windows.Forms.RichTextBox _richTextBox;
+        private System.Windows.Forms.GroupBox _groupBoxString;
+        private System.Windows.Forms.Button _buttonOk;
+        private System.Windows.Forms.Button _buttonCancel;
+        private System.Windows.Forms.Button _buttonInsert;
+        private System.Windows.Forms.Button _buttonEdit;
+        private System.Windows.Forms.Label _labelDescription;
+        private System.Windows.Forms.Panel _panelInsertEditButtons;
+        private System.Windows.Forms.Panel _panelOkCancelButtons;
+        private System.Windows.Forms.Panel _panelTopContent;
+    }
+}

commit 92238b7c6f8b173e8d424fabf1ac4d13ade32916

Fixed bug introduced in 5fc2783d59c5b118c033b331e88b1e58da7ac213 - you cannot use winforms.designer.sdk outside of designer assemblies.

src/WinForms.DataVisualization.Designer.ClientServerProtocol/Endpoints/KeywordsStringEditor/KeywordInfoDPO.cs
@@ -1,0 +1,36 @@
+﻿using System.Windows.Forms.DataVisualization.Charting.Utilities;
+
+using Microsoft.DotNet.DesignTools.Protocol.DataPipe;
+
+namespace WinForms.DataVisualization.Designer.Protocol.Endpoints
+{
+    internal record KeywordInfoDPO : KeywordInfo, IDataPipeObject
+    {
+        public KeywordInfoDPO() { }
+        public KeywordInfoDPO(KeywordInfo keywordInfo) : base(keywordInfo) { }
+
+        public void ReadProperties(IDataPipeReader reader)
+        {
+            Name = reader.ReadString(nameof(Name));
+            Keyword = reader.ReadString(nameof(Keyword));
+            KeywordAliases = reader.ReadString(nameof(KeywordAliases));
+            Description = reader.ReadString(nameof(Description));
+            AppliesToTypes = reader.ReadString(nameof(AppliesToTypes));
+            AppliesToProperties = reader.ReadString(nameof(AppliesToProperties));
+            SupportsFormatting = reader.ReadBoolean(nameof(SupportsFormatting));
+            SupportsValueIndex = reader.ReadBoolean(nameof(SupportsValueIndex));
+        }
+
+        public void WriteProperties(IDataPipeWriter writer)
+        {
+            writer.Write(nameof(Name), Name);
+            writer.Write(nameof(Keyword), Keyword);
+            writer.Write(nameof(KeywordAliases), KeywordAliases);
+            writer.Write(nameof(Description), Description);
+            writer.Write(nameof(AppliesToTypes), AppliesToTypes);
+            writer.Write(nameof(AppliesToProperties), AppliesToProperties);
+            writer.Write(nameof(SupportsFormatting), SupportsFormatting);
+            writer.Write(nameof(SupportsValueIndex), SupportsValueIndex);
+        }
+    }
+}

src/WinForms.DataVisualization.Designer.ClientServerProtocol/Endpoints/KeywordsStringEditor/KeywordsStringEditorEditValueResponse.cs
@@ -3,14 +3,12 @@
 using Microsoft.DotNet.DesignTools.Protocol.DataPipe;
 using Microsoft.DotNet.DesignTools.Protocol.Endpoints;
 
-using System.Windows.Forms.DataVisualization.Charting.Utilities;
-
 namespace WinForms.DataVisualization.Designer.Protocol.Endpoints
 {
     public class KeywordsStringEditorEditValueResponse : Response
     {
         public bool IsEmpty => RegisteredKeywords is null && MaxYValueNumber == 0;
-        internal IReadOnlyList<KeywordInfo>? RegisteredKeywords { get; private set; }
+        internal IReadOnlyList<KeywordInfoDPO>? RegisteredKeywords { get; private set; }
         public int MaxYValueNumber { get; private set; }
 
 
@@ -18,7 +16,7 @@ namespace WinForms.DataVisualization.Designer.Protocol.Endpoints
 
         public KeywordsStringEditorEditValueResponse(IDataPipeReader reader) : base(reader) { }
 
-        internal KeywordsStringEditorEditValueResponse(IReadOnlyList<KeywordInfo>? registeredKeywords, int maxYValueNumber)
+        internal KeywordsStringEditorEditValueResponse(IReadOnlyList<KeywordInfoDPO>? registeredKeywords, int maxYValueNumber)
         {
             RegisteredKeywords = registeredKeywords;
             MaxYValueNumber = maxYValueNumber;
@@ -26,7 +24,7 @@ namespace WinForms.DataVisualization.Designer.Protocol.Endpoints
 
         protected override void ReadProperties(IDataPipeReader reader)
         {
-            RegisteredKeywords = reader.ReadDataPipeObjectArray<KeywordInfo>(nameof(RegisteredKeywords));
+            RegisteredKeywords = reader.ReadDataPipeObjectArray<KeywordInfoDPO>(nameof(RegisteredKeywords));
             MaxYValueNumber = reader.ReadInt32(nameof(MaxYValueNumber));
         }
 

src/WinForms.DataVisualization.Designer.Server/KeywordsStringEditorEditValueHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms.DataVisualization.Charting;
 using System.Windows.Forms.DataVisualization.Charting.Utilities;
 
@@ -83,6 +84,13 @@ internal class KeywordsStringEditorEditValueHandler : RequestHandler<KeywordsStr
             }
         }
 
-        return new KeywordsStringEditorEditValueResponse((chart?.GetService(typeof(KeywordsRegistry)) as KeywordsRegistry)?.registeredKeywords.AsReadOnly(), maxYValueNumber);
+        var registeredKeywords = (chart?.GetService(typeof(KeywordsRegistry)) as KeywordsRegistry)?.registeredKeywords;
+        if (registeredKeywords is not null)
+        {
+            var registeredKeywordsDPO = registeredKeywords.Select(kw => new KeywordInfoDPO(kw)).ToList().AsReadOnly();
+            return new KeywordsStringEditorEditValueResponse(registeredKeywordsDPO, maxYValueNumber);
+        }
+
+        return new KeywordsStringEditorEditValueResponse();
     }
 }

src/WinForms.DataVisualization.Utilities/KeywordsRegistry.cs
@@ -41,8 +41,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 
-using Microsoft.DotNet.DesignTools.Protocol.DataPipe;
-
 namespace System.Windows.Forms.DataVisualization.Charting.Utilities;
 
 /// <summary>
@@ -107,7 +105,7 @@ internal class KeywordsRegistry : IServiceProvider
     /// </summary>
     /// <param name="serviceType">Service type to get.</param>
     /// <returns>Custom properties registry service.</returns>
-    [EditorBrowsableAttribute(EditorBrowsableState.Never)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
     object IServiceProvider.GetService(Type serviceType)
     {
         if (serviceType == typeof(KeywordsRegistry))
@@ -346,7 +344,7 @@ internal class KeywordsRegistry : IServiceProvider
 /// Description, list of data types and properties it 
 /// applies to and other information.
 /// </summary>
-internal class KeywordInfo : IDataPipeObject
+internal record KeywordInfo
 {
     #region Public Fields
 
@@ -444,7 +442,7 @@ internal class KeywordInfo : IDataPipeObject
     /// Returns a String that represents the current keyword Information.
     /// </summary>
     /// <returns>Returns keyword name.</returns>
-    public override string ToString()
+    public sealed override string ToString()
     {
         return this.Name;
     }
@@ -473,30 +471,5 @@ internal class KeywordInfo : IDataPipeObject
             return new string[] { this.Keyword };
         }
     }
-
-    public void ReadProperties(IDataPipeReader reader)
-    {
-        Name = reader.ReadString(nameof(Name));
-        Keyword = reader.ReadString(nameof(Keyword));
-        KeywordAliases = reader.ReadString(nameof(KeywordAliases));
-        Description = reader.ReadString(nameof(Description));
-        AppliesToTypes = reader.ReadString(nameof(AppliesToTypes));
-        AppliesToProperties = reader.ReadString(nameof(AppliesToProperties));
-        SupportsFormatting = reader.ReadBoolean(nameof(SupportsFormatting));
-        SupportsValueIndex = reader.ReadBoolean(nameof(SupportsValueIndex));
-    }
-
-    public void WriteProperties(IDataPipeWriter writer)
-    {
-        writer.Write(nameof(Name), Name);
-        writer.Write(nameof(Keyword), Keyword);
-        writer.Write(nameof(KeywordAliases), KeywordAliases);
-        writer.Write(nameof(Description), Description);
-        writer.Write(nameof(AppliesToTypes), AppliesToTypes);
-        writer.Write(nameof(AppliesToProperties), AppliesToProperties);
-        writer.Write(nameof(SupportsFormatting), SupportsFormatting);
-        writer.Write(nameof(SupportsValueIndex), SupportsValueIndex);
-    }
-
     #endregion // Methods
 }

src/WinForms.DataVisualization.Utilities/Utilities.csproj
@@ -2,7 +2,6 @@
 	<PropertyGroup>
 		<TargetFramework>netstandard2.0</TargetFramework>
 		<UseWindowsForms>true</UseWindowsForms>
-		<UseDesignerSdk>true</UseDesignerSdk>
 		<LangVersion>latest</LangVersion>
 		<AssemblyName>WinForms.DataVisualization.Utilities</AssemblyName>
 		<Nullable>enable</Nullable>